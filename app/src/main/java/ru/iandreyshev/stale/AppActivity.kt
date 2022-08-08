b041ad372777b3a42d272f5bc4e5fe14d75ff86c
ivan.andreyshev <ivan.andreyshev@ispring.com>
2022-08-08 22:32:06 +0300

    Поправил навигацию с нижним меню

--- app/src/main/java/ru/iandreyshev/stale/AppActivity.kt
+++ app/src/main/java/ru/iandreyshev/stale/AppActivity.kt
@@ -2,14 +2,40 @@ package ru.iandreyshev.stale
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import ru.iandreyshev.stale.domain.calc.CalcResultUseCase
-import ru.iandreyshev.stale.domain.core.Member
-import ru.iandreyshev.stale.domain.core.Transaction
-import ru.iandreyshev.stale.domain.core.TransactionParticipants
+import androidx.core.view.isVisible
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import ru.iandreyshev.stale.databinding.ActivityMainBinding
+import ru.iandreyshev.stale.ui.utils.uiLazy
+import ru.iandreyshev.stale.ui.utils.viewBindings
+import timber.log.Timber
 
 class AppActivity : AppCompatActivity() {
+
+    private val mBinding by viewBindings(ActivityMainBinding::bind)
+    private val mNavController by uiLazy {
+        (supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment)
+            .navController
+    }
+    private val mDestinationsWithMenu = setOf(
+        R.id.paymentListDest,
+        R.id.archiveDest,
+        R.id.settingsDest
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initMenu()
     }
+
+    private fun initMenu() {
+        mBinding.menuView.setupWithNavController(mNavController)
+        mNavController.addOnDestinationChangedListener { _, destination, _ ->
+            mBinding.menuView.isVisible = destination.id in mDestinationsWithMenu
+        }
+    }
+
 }
